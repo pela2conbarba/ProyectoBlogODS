9534d62e07695adbc4ba4fc1d395597ebcedddca
sebo7 <sebadlmg@gmail.com>
2021-12-18 18:26:30 -0300

    17 posteos 2.0

--- posts/views.py
+++ posts/views.py
@@ -17,7 +17,7 @@ class PostsFeedView(ListView):
     template_name = 'posts/index.html'
     model = Post
     ordering = ('-created',)
-    paginate_by = 10
+    paginate_by = 18
     context_object_name = 'posts'
     queryset = Post.objects.filter(is_draft=False)
 
